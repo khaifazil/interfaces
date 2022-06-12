75a9d9d54694f292bf248f7df4bc4fa5d4905e42
Khai Fazil <khairal.fazil@gmail.com>
2022-06-13 05:05:53 +0800

    completed excercise

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import "fmt"
 
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	//custom logic for english bot
+	return "Hi There"
+}
+
+func (spanishBot) getGreeting() string {
+	//custom logic for spanish bot
+	return "Hola!"
 }
